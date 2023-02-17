ddb00c61fd791427cbd9ca0055cbee359c4d3fa2
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-02-17 01:31:10 +0000

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 const API_KEY = '55c2fecd650724ce09bf1acb1efb65d5';
+
 const BASE_URL = 'https://api.openweathermap.org';
 
+
+//Sets up init state of Weather App
 let state = {
-  //start here by inputting what you want to pull from the api (temp, city, ...)
-  city: null, //response.data.name
+  city: null,
   temperature: {
-    kelvin: null, //response.data.main.temp
-    fahrenheit: null, //will have to run through function
-    celsius: null, //will have to run through function
+    kelvin: null,
+    fahrenheit: null,
+    celsius: null,
   },
-  weatherCondition: null, //response.data.weather.main
-  weatherDescription: null, //response.data.weather.description
-  weatherIcon: null, //response.data.weather.icon
+  weatherCondition: null,
+  weatherDescription: null, 
+  weatherIcon: null, 
   zip: '47150',
   errorStatus: false,
-  errorMessage: "",
-  //hidden vs shown upon click
-}
+  errorMessage: ""
+};
+
 
 //code to set up input/start screen
 window.onload = function createStartPage() {
@@ -27,221 +29,194 @@ window.onload = function createStartPage() {
 
   //Parent div for the title
   let titleDiv = document.createElement('div');
-  main.appendChild(titleDiv);
   titleDiv.className = 'display-1 m-5 text-center'
+  main.appendChild(titleDiv);
 
   //creation of the App Title
   let titleTag = document.createElement('h1');
-  let titleText = document.createTextNode("Weather App");
-  titleDiv.appendChild(titleTag.appendChild(titleText));
+  titleTag.innerText = "Weather App";
   titleTag.className = "display-1";
+  titleDiv.appendChild(titleTag);
+  
 
   //Parent div for the input and button
   let inputDiv = document.createElement('div');
+  inputDiv.className = 'text-center';
   main.appendChild(inputDiv);
-  // inputDiv.id = "form";
-  inputDiv.className = 'text-center'
+  
 
   //creation of zip code input field
   let zipInput = document.createElement('input');
   zipInput.type = "text";
   zipInput.id = 'zipCodeBox';
   zipInput.className = "p-1 me-5";
-  zipInput.minLength = "5";
-  zipInput.maxLength = "5";
   zipInput.placeholder = "Enter 5 digit zip code"
   inputDiv.appendChild(zipInput);
 
-  //creation of submit button
+  //creation of submit button in html
   let submitBtn = document.createElement('button');
   submitBtn.type = 'submit';
+  submitBtn.innerText = "Get Weather";
   submitBtn.className = "btn btn-info mb-2";
-  let btnText = document.createTextNode("Get Weather");
-  submitBtn.appendChild(btnText);
-  inputDiv.appendChild(submitBtn);
+  inputDiv.appendChild(submitBtn);  
 
-  // let errorDiv = document.createElement('div');
-  // errorDiv.className = "me-5 mb-5";
-  // main.appendChild(errorDiv);
-  // errorDiv.style.display = `${state.hideShowMessage}`;
-  // let errorMessageDiv = document.createElement('div');
-  // errorDiv.appendChild(errorMessageDiv);
-  // let errorMessage = document.createTextNode(`${state.error}`);
-  // errorMessageDiv.appendChild(errorMessage); 
-  // errorMessageDiv.className = "text-center pe-5 me-5 text-danger h6";
-
+  //creation of error message container in html
   let errorDiv = document.createElement('div');
   errorDiv.className = "me-5 mb-5";
   errorDiv.id = 'errorDiv';
-  main.appendChild(errorDiv);
   errorDiv.style.display = `None`;
+  main.appendChild(errorDiv);
+
+  //creation of error message
   let errorMessageDiv = document.createElement('div');
-  errorDiv.appendChild(errorMessageDiv);
-  let errorMessageTextNode = document.createTextNode(`${state.error}`);
-  console.log(errorMessageTextNode);
-  errorMessageDiv.appendChild(errorMessageTextNode); 
+  errorMessageDiv.innerText = `${state.error}`;
   errorMessageDiv.className = "text-center pe-5 me-5 text-danger h6";
   errorMessageDiv.id = 'errorMessageTextNode';
+  errorDiv.appendChild(errorMessageDiv);
 
+  /*calls function that creates the html to store api
+  weather data - set to display none until after button click */
   renderWeatherPane();
 
-  //calls getWeather on button click
-  // inputDiv.addEventListener('click', getWeather);
-  // state.zip = document.getElementsByTagName('input').value;
+  //calls function that handles button click - either error or success
   submitBtn.addEventListener('click', handleGetWeatherBtnClick);
-
-
-}
+};
 
 
 function handleGetWeatherBtnClick() {
+  //sets zip code input to reusable variable
   let newZip = document.getElementById('zipCodeBox').value;
+
   //reset error state
   state.errorStatus = false;
   state.errorMessage = "";
-  getWeatherData(newZip);
 
-// if (state.errorStatus === true) {
-//   let errorDiv = document.getElementById('errorDiv');
-//   errorDiv.style.display = 'block';
-//   let errorMessageDiv = document.getElementById('errorMessage');
-//   errorMessageDiv.innerHTML = `Error: ${state.error}`;
-// } else {
-//   populateWeather(newZip);
-// }
-
-  
+  //calls function that grabs weather data based on zip code
+  getWeatherData(newZip);  
 };
 
+
+//API FUNCTION - takes in zip code obtained from button click
 function getWeatherData(zip) {
-    let options = {
-      baseURL: BASE_URL,
-      params: {
-        zip: zip,
-        country: 'us',
-        appid: API_KEY
-      }
+  let options = {
+    baseURL: BASE_URL,
+    params: {
+      zip: zip,
+      country: 'us',
+      appid: API_KEY
     }
-    
-    axios.get(`/data/2.5/weather`, options)
-      .then(function (response) {
-        console.log(response);
-        state.city = response.data.name;
-        state.temperature.kelvin = response.data.main.temp;
-        state.weatherCondition = response.data.weather[0].main;
-        state.weatherDescription = response.data.weather[0].description;
-        state.weatherIcon = response.data.weather[0].icon;
-        console.log(state);
-      })
-      .catch(function (error) {
-        console.log(error);
-        state.errorStatus = true;
-        state.error = error.response.data.message;
-      })
-      .finally(function () {
-        // always executed
-        displayWeatherDataResult();
-    });
   }
-
-    //create function that changes kelvin into F and C
-    function convertToF(num) {
-      let f = Math.floor((num - 273.15) * 9/5 + 32)
-      return f;
-    };
-  
-    function convertToC(num) {
-      let c = Math.floor(num - 273.15);
-      return c;
-    };
-
-
-
-   
     
-  //Set up the html elements for the information from the API after button click! 
-function renderWeatherPane() {
+  axios.get(`/data/2.5/weather`, options)
+    .then(function (response) {
+      console.log(response);
+      state.city = response.data.name;
+      state.temperature.kelvin = response.data.main.temp;
+      state.weatherCondition = response.data.weather[0].main;
+      state.weatherDescription = response.data.weather[0].description;
+      state.weatherIcon = response.data.weather[0].icon;
+      console.log(state);
+    })
+    .catch(function (error) {
+      console.log(error);
+      state.errorStatus = true;
+      state.error = error.response.data.message;
+    })
+    .finally(function () {
+      // always executed
+      displayWeatherDataResult(); //function handles both error and success cases
+  });
+};
 
+
+//handles conversion of kelvin to fahrenheit
+function convertToF(num) {
+  let f = Math.floor((num - 273.15) * 9/5 + 32)
+  return f;
+};
+
+
+//handles conversion of kelvin to celsius
+function convertToC(num) {
+  let c = Math.floor(num - 273.15);
+  return c;
+};
+
+    
+/*Set up the html elements for the information from the API
+after button click! Function is called in initial setup function */
+function renderWeatherPane() {
+  //sets up html that contains all weather components
   let weatherDiv = document.createElement('div');
-  main.appendChild(weatherDiv);
   weatherDiv.className = "m-5 text-center";
   weatherDiv.style.border = "solid 2px";
   weatherDiv.id = "weatherDiv";
   weatherDiv.style.display = "none";
+  main.appendChild(weatherDiv);
 
+  //creation of html div that holds the city name
   let cityDiv = document.createElement('div');
   cityDiv.className = "display-3 mt-5";
-  weatherDiv.appendChild(cityDiv);
-  let cityTag = document.createElement('h2'); //wtf
-  let cityText = document.createTextNode(`City`); //wwtyf
-  cityDiv.appendChild(cityTag.appendChild(cityText)); //help me
+  cityDiv.innerText = "City";
   cityDiv.id = 'cityDiv';
-  cityTag.className;
+  weatherDiv.appendChild(cityDiv);
 
- 
+  //sets up div that contains the 3 different temperatures 
   let tempDiv = document.createElement('div');
   tempDiv.className = "d-flex flex-row justify-content-evenly display-5 mt-5 mb-5";
   weatherDiv.appendChild(tempDiv);
+
+  //Kelvin Column
   let kelvinDiv = document.createElement('div');
   kelvinDiv.className = 'col-4';
   kelvinDiv.id = 'kelvinDiv';
+  kelvinDiv.innerText = "K";
   tempDiv.appendChild(kelvinDiv);
-  let kText = document.createTextNode(`K`);
-  kelvinDiv.appendChild(kText);
+
+  //Fahrenheit Column
   let fahrenheitDiv = document.createElement('div');
   fahrenheitDiv.className = 'col-4';
   fahrenheitDiv.id = 'fahrenheitDiv';
+  fahrenheitDiv.innerText = "F";
   tempDiv.appendChild(fahrenheitDiv);
-  let fText = document.createTextNode(`F`);
-  fahrenheitDiv.appendChild(fText);
+
+  //Celsius Column
   let celsiusDiv = document.createElement('div');
   celsiusDiv.className = 'col-4';
   celsiusDiv.id = 'celsiusDiv';
+  celsiusDiv.innerText = "C";
   tempDiv.appendChild(celsiusDiv);
-  let cText = document.createTextNode(`C`);
-  celsiusDiv.appendChild(cText);
   
-
+  //Div that contains the weather condition and Description
   let wConditionDiv = document.createElement('div');
   weatherDiv.appendChild(wConditionDiv);
   wConditionDiv.className;
+
+  //Div that holds main weather condition
   let conDiv = document.createElement('div');
   wConditionDiv.appendChild(conDiv);
-  conDiv.className;
+  conDiv.innerText = "Weather Condition";
   conDiv.id = 'conDiv';
-  let conText = document.createTextNode(`weather condition`);
-  conDiv.appendChild(conText);
 
-  //outer div for weather condition
-  let wConditionDesDiv = document.createElement('div');
-  weatherDiv.appendChild(wConditionDesDiv);
-  wConditionDesDiv.className;
   //inner div for weather condition description
   let conDesDiv = document.createElement('div');
-  wConditionDesDiv.appendChild(conDesDiv);
-  conDesDiv.className;
+  conDesDiv.innerText = "Weather Description";
   conDesDiv.id = 'conDesDiv';
-  //sets text of inner weather condition description div
-  let conDesText = document.createTextNode(`des`);
-  conDesDiv.appendChild(conDesText);
+  wConditionDiv.appendChild(conDesDiv);
 
-  //other info div
-  //add the icon here
+  //creates div that holds the weather icon img
   let weatherIconDiv = document.createElement('div');
   weatherDiv.appendChild(weatherIconDiv);
-  weatherIconDiv.className;
+  
+  //creation of api weather icon in html
   let weatherIconImg = document.createElement('img');
-  weatherIconDiv.appendChild(weatherIconImg);
-  weatherIconImg.src;
+  weatherIconImg.src = "";
   weatherIconImg.id = 'weatherIconImg';
-}
+  weatherIconDiv.appendChild(weatherIconImg);
+};
 
-// function setZip() {
-//   state.zip = "42303";
-//   // state.zip;
-//   // return state.zip;
-// }
 
+//function that will display the results of getWeatherData - both if it fails or succeeds
 function displayWeatherDataResult() {
   if (state.errorStatus === true) {
     let errorDiv = document.getElementById('errorDiv');
@@ -263,12 +238,4 @@ function displayWeatherDataResult() {
     let errorDiv = document.getElementById('errorDiv');
     errorDiv.style.display = 'none';
   }
-}
-
-
-// function validateZipCode(newZip) {
-//   //Code to test if newZip
-
-//   return false;
-// }
-// document.getElementById('form').addEventListener('submit', setZip);
+};
