321f5188f12ef2777ce0492734b5e2ba77237da9
Katy <katysimpson90@gmail.com>
2023-05-05 09:29:33 -0400

    adding react

---
+++ my_weather/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+    <div>
+      Hi!
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
